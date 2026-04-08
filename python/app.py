3ac778086f902df44a5c7ebf610285798e26e6a8
Anne L'Hôte <anne.lhote@gmail.com>
2026-04-08 14:40:57 +0200

    ftae(python): Switch from Flask to FastAPI

--- python/app.py
+++ python/app.py
@@ -1,10 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import json
-from flask import Flask
 from swagger_ui import api_doc
 
 from utils import get_collection_doc, filter_data, read_data, write_data
 
-app = Flask(__name__)
+app = FastAPI(
+    title="datApi",
+    description="From data to API",
+    version="0.0.0"
+)
 
 try:
     with open("config.json", "r") as file:
@@ -25,26 +30,24 @@ doc = {
     },
     "paths": merged,
 }
-
 api_doc(app, config=doc, url_prefix="/doc", title="API doc")
 
-@app.route("/")
-def hello_world():
+@app.get("/", response_class=HTMLResponse)
+def home():
     # TODO: Replace it by a list of available collections , and link to the dedicated page
-    return "<p>Hello, World!</p>"
+    # return "<p>Hello, World!</p>"
+    list = [f"<li><a href=\"./api/doc/{collection}\">{collection}</a></li>" for collection in config["collections"]]
+    return f"<h1>datApi</h1><ul>{''.join(list)}</ul>"
 
-@app.route("/read")
+@app.get("/read")
 def read():
     data = read_data(config["collections"][0])
-    print(len(data))
     filtered_data = filter_data(data)
-    print(len(filtered_data))
     write_data(config["collections"][0], filtered_data)
-    print("done")
     return "<p>Job done</p>"
 
-@app.route("/api/doc/<collection>")
-def doc_collection(collection):
+@app.get("/api/doc/{collection}")
+def json_collection(collection):
     if collection not in config["collections"]:
         return f"The collection \"{collection}\" does not exist", 500
     paths = get_collection_doc(collection)
@@ -57,3 +60,19 @@ def doc_collection(collection):
         },
         "paths": paths,
     }, 200
+
+@app.get("/doc/{collection}")
+def swagger_collection(collection):
+    if collection not in config["collections"]:
+        return f"The collection \"{collection}\" does not exist", 500
+    paths = get_collection_doc(collection)
+    doc = {
+        "openapi": "3.0.0",
+        "info": {
+        "title": f"datapi - {collection}",
+        "version": "1.0.0",
+        "description": f"Documentation générée automatiquement à partir d'un échantillon de 20 documents de la collection **{collection}**.",
+        },
+        "paths": paths,
+    }
+    api_doc(app, config=doc, url_prefix=f"/doc/{collection}", title="API doc")
